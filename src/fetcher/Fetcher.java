90d973ccab540047c207e1d1a1ed29c592a865b0
Yaniv Gabay <yanivga@std.hac.ac.il>
2024-04-19 23:24:37 +0300

    more docs

--- src/fetcher/Fetcher.java
+++ src/fetcher/Fetcher.java
@@ -2,6 +2,15 @@ package fetcher;
 
 import model.FetchResult;
 
+/**
+ * Interface for fetching content from a URL.
+ */
 public interface Fetcher {
+    /**
+     * Fetches content from the given URL.
+     *
+     * @param urlString The URL to fetch content from.
+     * @return The result of the fetch operation.
+     */
     FetchResult fetch(String urlString);
 }
--- src/fetcher/FetcherFactory.java
+++ src/fetcher/FetcherFactory.java
@@ -4,9 +4,15 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Supplier;
 
+/**
+ * Factory class for creating fetcher instances based on content type.
+ */
 public class FetcherFactory {
     private final Map<String, Supplier<Fetcher>> fetcherMap;
 
+    /**
+     * Constructs a FetcherFactory and registers default fetchers.
+     */
     public FetcherFactory() {
         this.fetcherMap = new HashMap<>();
         // Register default fetchers
@@ -15,10 +21,23 @@ public class FetcherFactory {
         // registerFetcher("text/", TextFetcher::new);
     }
 
+    /**
+     * Registers a fetcher supplier for the given content type prefix.
+     *
+     * @param key             The content type prefix.
+     * @param fetcherSupplier The supplier to create fetcher instances.
+     */
     public void registerFetcher(String key, Supplier<Fetcher> fetcherSupplier) {
         fetcherMap.put(key, fetcherSupplier);
     }
 
+    /**
+     * Creates a fetcher instance based on the content type.
+     *
+     * @param contentType The content type.
+     * @return A fetcher instance.
+     * @throws IllegalArgumentException If the content type is not supported.
+     */
     public Fetcher createFetcher(String contentType) {
         // Iterate over keys to find a match, allowing for partial matches (e.g., startsWith)
         for (Map.Entry<String, Supplier<Fetcher>> entry : fetcherMap.entrySet()) {
